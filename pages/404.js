97b82e15dd8e674dedb23541dcc784d8bd38219d
Ariel Favaro <arieltainan@gmail.com>
2021-07-18 22:01:35 -0300

    page 404 text-center

--- pages/404.js
+++ pages/404.js
@@ -12,8 +12,8 @@ export default function Custom404() {
             </Head>
             <FloatingParticles />
             <div className="flex flex-col items-center justify-center min-h-screen">
-                <h1 className="text-5xl text-red-500 font-black">Ooops - 404</h1>
-                <h3 className="font-extralight text-5xl text-red-500">Houston, we have a problem!</h3>
+                <h1 className="text-center text-5xl text-red-500 font-black">Ooops - 404</h1>
+                <h3 className="text-center font-extralight text-5xl text-red-500">Houston, we have a problem!</h3>
                 <Link href="/">
                     <a className="bg-primary rounded-full my-4 px-4 py-2 text-white">Início</a>
                 </Link>
