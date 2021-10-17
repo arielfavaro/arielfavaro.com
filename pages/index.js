1c98d33806e3432fde13f35dd6d69469ee7c3902
Ariel Favaro <arieltainan@gmail.com>
2021-10-17 19:56:41 -0300

    changed description

--- pages/index.js
+++ pages/index.js
@@ -11,13 +11,8 @@ export default function Home() {
         <div>
             <FloatingParticles />
             <main className={styles.main}>
-                <h1 className={styles.mainTitle}>GET READY!</h1>
-                <h3 className={styles.description}>Desenvolvimento Web</h3>
-                <h3 className="text-white max-w-sm text-center">Desenvolvimento de aplicações web utilizando as mais recentes tecnologias no mercado.</h3>
-                {/* <h3 className={styles.description}>Entre em contato</h3> */}
-                {/* <Link href="mailto:contato@arielfavaro.com">
-                    <a target="_blank" className={styles.email}>contato@arielfavaro.com</a>
-                </Link> */}
+                <h1 className={styles.mainTitle}>&lt;Hello /&gt;</h1>
+                <h3 className={styles.description}>Desenvolvimento de aplicações Web de ponta que entregam valor e resultados</h3>
                 <div className={styles.socialWrapper}>
                     <Link href="https://linkedin.com/in/arielfavaro">
                         <a target="_blank" className={styles.social}><FaLinkedinIn /></a>
