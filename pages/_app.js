b3fd3e32c7bcbd8230145480fcc464077cbbafa1
Ariel Favaro <arieltainan@gmail.com>
2022-01-02 10:56:08 -0300

    changed title and description

--- pages/_app.js
+++ pages/_app.js
@@ -9,12 +9,12 @@ function MyApp({ Component, pageProps }) {
                 <meta charSet="utf-8" />
                 <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
-                <meta name="description" content="Desenvolvimento Web e Mobile" />
-                <meta name="keywords" content="web, development, seo, ecommerce, loja virtual, loja online, desenvolvido de sistemas, desenvolvido de sites, desenvolvido de app, website, webdesign, site, publicidade, marketing, desenvolvimento site, desenvolvimento, desenvolver site, blog, aplicativos, aplicativo, agencia publicidade, agencia, porto ferreira, sao carlos, sao paulo, dev, ariel, ariel favaro, site responsivo" />
+                <meta name="description" content="Desenvolvimento de aplicações Web de ponta que entregam valor e resultados" />
+                <meta name="keywords" content="web, development, seo" />
                 <link key="canonical" rel="canonical" href="https://arielfavaro.com/" />
                 {/* <link rel="icon" href="/favicon.ico" /> */}
 
-                <title>Ari Dev - Desenvolvimento Web e Mobile</title>
+                <title>Ari Dev - Desenvolvimento Web</title>
 
                 {/* <link rel="manifest" href="/manifest.json" />
 
