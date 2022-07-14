1f978cfc82266251a8a275cb04de98e9f69d0add
Ariel Favaro <arieltainan@gmail.com>
2022-07-14 10:17:05 -0300

    added more projects, styles adjustments

--- pages/meus-projetos.js
+++ pages/meus-projetos.js
@@ -9,16 +9,33 @@ export default function MeusProjetos() {
                 <title>Meus Projetos - Desenvolvedor Web - Ariel</title>
             </Head>
             <section className="container">
-                <h1 className="text-center text-5xl text-primary font-black mb-3">Meus Projetos</h1>
+                <h1 className="text-center text-5xl text-primary font-black">Meus Projetos</h1>
 
-                <div className="flex flex-wrap justify-center gap-4 py-8">
+                <div className="flex flex-wrap justify-center gap-4 pt-4 pb-8">
                     <CardProject
                         title="Gerador de Link para whats"
                         description="Crie link curto para whats com QR Code de maneira fácil e compartilhe com seus clientes."
                         link="https://app.linkwpp.com/"
                         icon="geradorlinkwhatsapp-icon.webp"
                     />
+                    <CardProject
+                        title="GitHub Badges"
+                        description="Exibir contagem de visitas em seu repositório no GitHub."
+                        link="http://another-github-badges.ari.dev.br/"
+                        icon="github-mark-light-120px-plus.webp"
+                    />
                     <CardProjectComingSoon />
+                </div>
+
+                <h2 className="text-center text-3xl text-primary font-black">Showcase</h2>
+
+                <div className="flex flex-wrap justify-center gap-4 pt-4 pb-8">
+                    <CardProject
+                        title="Caneca Flutuante"
+                        description="Caneca flutuante em 3D, com ThreeJs, React Three Fiber."
+                        link="http://floating-mug.vercel.app/"
+                        icon="caneca-3d.webp"
+                    />
                 </div>
             </section>
         </div>
