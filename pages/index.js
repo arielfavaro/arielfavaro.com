aa02d9353b4157b72fb06a510d3a68fa7250d886
Ariel Favaro <arieltainan@gmail.com>
2022-07-24 15:27:26 -0300

    updated contacts info

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Link from 'next/link'
 import styles from '@/styles/Home.module.css'
 import { FaLinkedinIn, FaGithub } from 'react-icons/fa'
 import { HiMail } from "react-icons/hi"
+import { contactInfo } from '@/lib/contactInfo'
 
 export default function Home() {
     return (
@@ -10,13 +11,13 @@ export default function Home() {
                 <h1 className={styles.mainTitle}>&lt;Hello /&gt;</h1>
                 <h2 className={styles.description}>Desenvolvimento de aplicações Web de ponta que entregam valor e resultados</h2>
                 <div className={styles.socialWrapper}>
-                    <Link href="https://linkedin.com/in/arielfavaro">
+                    <Link href={contactInfo.linkedin}>
                         <a target="_blank" rel="noopener" className={styles.social} aria-label="Linkedin"><FaLinkedinIn /></a>
                     </Link>
-                    <Link href="https://github.com/arielfavaro">
+                    <Link href={contactInfo.github}>
                         <a target="_blank" rel="noopener" className={styles.social} aria-label="GitHub"><FaGithub /></a>
                     </Link>
-                    <Link href="mailto:contato@arielfavaro.com">
+                    <Link href={`mailto:${contactInfo.email}`}>
                         <a target="_blank" rel="noopener" className={styles.social} aria-label="Email"><HiMail /></a>
                     </Link>
                 </div>
